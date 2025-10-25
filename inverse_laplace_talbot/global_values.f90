7b128e297b7f48f4ad801c69e66a5b977f373c01
Anuvab2002 <anuvabpanda2002@gmail.com>
2025-10-25 19:47:51 +0530

    contains global parameters and their values

---
+++ inverse_laplace_talbot/global_values.f90
@@ -0,0 +1,8 @@
+module global_values_function
+!
+  real(8), parameter    :: tolerance_zero = 0.00000001d0
+  real(8), parameter    :: pi = 4.0d0*atan(1.0d0)
+  real(8), parameter    :: min_nonzero = 0.000001d0
+  complex(8), parameter :: iota = (0.0d0, 1.0d0)
+!
+end module global_values_function
